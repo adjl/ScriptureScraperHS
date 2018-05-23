8c780820afeac69a5fc7b2fa4d183f0a51940764
Adei Josol <helena.josol@gmail.com>
2018-05-23 23:04:38 +0100

    Retrieve matched HTML

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.List
 import Data.Maybe
 import System.Environment
 import Text.HTML.Scalpel
@@ -7,25 +8,24 @@ import Text.HTML.Scalpel
 getURL :: [String] -> URL
 getURL [book, chapter, version] = "https://www.biblegateway.com/passage/?search=" ++ book ++ "+" ++ chapter ++ "&version=" ++ version
 
-scraper :: Scraper String [[String]]
+scraper :: Scraper String [String]
 scraper = chroot ("div" @: [hasClass "result-text-style-normal", hasClass "text-html"]) $ do
-    chroots "p" $ do
-        chroots ("span" @: [hasClass "text"]) $ do
-            verses <- text anySelector
-            return verses
+    chroots ("p" // "span" @: [hasClass "text"]) $ do
+        verses <- innerHTML anySelector
+        return verses
 
 bibleScraper :: [String] -> IO ()
 bibleScraper args = do
     results <- scrapeURL (getURL args) scraper
     let contents :: [String]
-        contents = concat $ fromMaybe [] $ results
+        contents = fromMaybe [] results
     if null contents then
         putStrLn "Could not retrieve contents."
     else do
         let fileName :: FilePath
-            fileName = foldl1 (++) args ++ ".txt"
+            fileName = foldl1 (++) args ++ "HTML.txt"
         putStrLn $ "Writing contents to file " ++ fileName ++ " ..."
-        mapM_ (appendFile fileName) contents
+        mapM_ (appendFile fileName) $ intersperse "\n" contents
 
 main :: IO ()
 main = getArgs >>= bibleScraper
