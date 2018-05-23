0ed6554520b34587dafa321817c73c4a821e808e
Adei Josol <helena.josol@gmail.com>
2018-05-23 23:31:51 +0100

    Tidy up

--- Main.hs
+++ Main.hs
@@ -6,26 +6,36 @@ import System.Environment
 import Text.HTML.Scalpel
 
 getURL :: [String] -> URL
-getURL [book, chapter, version] = "https://www.biblegateway.com/passage/?search=" ++ book ++ "+" ++ chapter ++ "&version=" ++ version
+getURL [book, chapter, version] =
+    "https://www.biblegateway.com/passage/?" ++
+    "search=" ++ book ++ "+" ++ chapter ++
+    "&version=" ++ version
+
+chapterSelector :: Selector
+chapterSelector = "div" @:
+    [hasClass "result-text-style-normal", hasClass "text-html"]
 
 scraper :: Scraper String [String]
-scraper = chroot ("div" @: [hasClass "result-text-style-normal", hasClass "text-html"]) $ do
+scraper = chroot chapterSelector $ do
     chroots ("p" // "span" @: [hasClass "text"]) $ do
-        verses <- innerHTML anySelector
-        return verses
+        innerHTML anySelector >>= return
 
-bibleScraper :: [String] -> IO ()
-bibleScraper args = do
+writeToFile :: FilePath -> [String] -> IO ()
+writeToFile fileName contents = do
+    putStrLn $ "Writing contents to file " ++ fileName ++ " ..."
+    mapM_ (appendFile fileName) $ intersperse "\n" contents
+
+chapterScraper :: [String] -> IO ()
+chapterScraper args = do
     results <- scrapeURL (getURL args) scraper
     let contents :: [String]
         contents = fromMaybe [] results
-    if null contents then
-        putStrLn "Could not retrieve contents."
+    if null contents then putStrLn "Could not retrieve contents."
     else do
         let fileName :: FilePath
             fileName = foldl1 (++) args ++ "HTML.txt"
-        putStrLn $ "Writing contents to file " ++ fileName ++ " ..."
-        mapM_ (appendFile fileName) $ intersperse "\n" contents
+        writeToFile fileName contents
 
 main :: IO ()
-main = getArgs >>= bibleScraper
+main = do
+    getArgs >>= chapterScraper
