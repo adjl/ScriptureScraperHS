249884a404c1b4e2db43d4f9e2fdb15b3df531fe
Adei Josol <helena.josol@gmail.com>
2018-05-25 00:57:17 +0100

    Add more regexes... Then find out POSIX regexes don't recognise non-greedy quantifiers

--- Main.hs
+++ Main.hs
@@ -5,14 +5,36 @@ import Data.Maybe
 import System.Environment
 import Text.HTML.Scalpel
 import Text.Regex
+import Text.Regex.PCRE ((=~))
 
 stripRegexes :: [Regex]
 stripRegexes = [
     mkRegex "<span\\s+class=\"chapternum\">[0-9]+\160*<\\/span>",
     mkRegex "<sup\\s+class=\"versenum\">[0-9]+\160*<\\/sup>",
     mkRegex "<sup\\s+class=\"crossreference\".+><\\/sup>",
-    mkRegex "<sup.+class=\"footnote\".+>.+<\\/sup>"
-]
+    mkRegex "<sup.+class=\"footnote\".+>.+<\\/sup>"]
+
+dashRegex :: Text.Regex.Regex
+dashRegex = mkRegex "—"
+
+emphRegex :: String
+emphRegex = "<i>(.+)<\\/i>"
+
+obliqRegex :: String
+obliqRegex = "<span\\s+class=\"oblique\">“(.+)”<\\/span>"
+
+dashReplace :: String -> String
+dashReplace rawString = subRegex dashRegex rawString "---"
+
+emphReplace :: String -> String
+emphReplace rawString = if matched then "\\emph{" ++ last group ++ "}" else rawString
+    where group = concat (rawString =~ emphRegex :: [[String]])
+          matched = length group > 1
+
+obliqReplace :: String -> String
+obliqReplace rawString = if matched then "``" ++ last group ++ "''" else rawString
+    where group = concat (rawString =~ obliqRegex :: [[String]])
+          matched = length group > 1
 
 selector :: Selector
 selector = "div" @:
@@ -29,7 +51,7 @@ getURL [book, chapter, version] =
     "search=" ++ book ++ "+" ++ chapter ++
     "&version=" ++ version
 
-stripHTML :: [Regex] -> [String] -> [String]
+stripHTML :: [Text.Regex.Regex] -> [String] -> [String]
 stripHTML []              strippedHTML = strippedHTML
 stripHTML (regex:regexes) rawHTML      = stripHTML regexes strippedHTML
     where strippedHTML :: [String]
