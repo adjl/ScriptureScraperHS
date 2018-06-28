3b180c26b716616cbcc810147deed59513f3b9d7
Adei Josol <helena.josol@gmail.com>
2018-06-28 23:48:05 +0100

    Clean up code

--- Main.hs
+++ Main.hs
@@ -1,12 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Functor
 import Text.HTML.Scalpel
+import Text.HTML.TagSoup
 
-scraper :: Scraper String [String]
-scraper = chroot ("div" @: [hasClass "result-text-style-normal", hasClass "text-html"]) $ do
-    chroots ("p" // "span" @: [hasClass "text"]) $ do
-        innerHTML anySelector >>= return
+-- TODO: ByteString
 
 getURL :: [String] -> URL
 getURL [book, chapter, version] = "https://www.biblegateway.com/passage/?" ++
     "search=" ++ book ++ "+" ++ chapter ++ "&version=" ++ version
+
+scraper :: Scraper String [String]
+scraper = chroot ("div" @: [hasClass "result-text-style-normal", hasClass "text-html"]) $ do
+    chroots ("p" // "span" @: [hasClass "text"]) $ do
+        innerHTML anySelector
+
+parseHTML :: Maybe [String] -> [Tag String]
+parseHTML = maybe [] (\s -> parseTags $ foldl1 (++) s)
+
+getTags :: URL -> IO [Tag String]
+getTags url = scrapeURL url scraper <&> parseHTML
