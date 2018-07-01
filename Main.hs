9a2f9ca0b3a41e05dbe3fcc3057aec9f4c8c2af3
Adei Josol <helena.josol@gmail.com>
2018-07-01 19:04:05 +0100

    Clean up filterTags_ function

--- Main.hs
+++ Main.hs
@@ -14,18 +14,6 @@ scraper = chroot ("div" @: [hasClass "result-text-style-normal", hasClass "text-
     chroots ("p" // "span" @: [hasClass "text"]) $ do
         innerHTML anySelector
 
-tagsToStrip :: [String]
-tagsToStrip = ["span", "sup"]
-
-oblique :: String
-oblique = "oblique"
-
-italic :: String
-italic = "i"
-
-isOblique :: Tag String -> Bool
-isOblique tag = fromAttrib "class" tag == oblique
-
 getTags :: URL -> IO [Tag String]
 getTags url = parseHTML <$> scrapeURL url scraper
 
@@ -38,14 +26,38 @@ filterTags = filterTags_ (False, "")
 filterTags_ :: (Bool, String) -> [Tag String] -> [Tag String]
 filterTags_ _ [] = []
 filterTags_ (False, _) (tag@(TagOpen name _):tags)
-    | isOblique tag         =                     filterTags_ (False, oblique) tags
-    | elem name tagsToStrip =                     filterTags_ (True,  name)    tags
-    | name == italic        = TagText "\\emph{" : filterTags_ (False, italic)  tags
+    | name == "i"    = openEmphTag : filteredTags (False, "i")
+    | isOblique tag  =               filteredTags (False, "o")
+    | stripElem name =               filteredTags (True,  name)
+    where
+        isOblique :: Tag String -> Bool
+        isOblique tag = fromAttrib "class" tag == "oblique"
+
+        stripElem :: String -> Bool
+        stripElem name = elem name ["span", "sup"]
+
+        filteredTags :: (Bool, String) -> [Tag String]
+        filteredTags state = filterTags_ state tags
+
+        openEmphTag :: Tag String
+        openEmphTag = TagText "\\emph{"
+
 filterTags_ (False, word) (TagClose _ : tags)
-    | word == oblique =               filterTags_ (False, "") tags
-    | word == italic  = TagText "}" : filterTags_ (False, "") tags
+    | word == "i" = closeEmphTag : filteredTags
+    | word == "o" =                filteredTags
+    where
+        filteredTags :: [Tag String]
+        filteredTags = filterTags_ (False, "") tags
+
+        closeEmphTag :: Tag String
+        closeEmphTag = TagText "}"
+
 filterTags_ (True, word) (TagClose name : tags)
     | word == name = filterTags_ (False, "") tags
+
 filterTags_ state@(stripMode, _) (tag:tags)
-    | stripMode =       filterTags_ state tags
-    | otherwise = tag : filterTags_ state tags
+    | stripMode =       filteredTags
+    | otherwise = tag : filteredTags
+    where
+        filteredTags :: [Tag String]
+        filteredTags = filterTags_ state tags
